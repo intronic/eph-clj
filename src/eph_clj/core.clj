8522f88bdcebf1e8bcd28394b1fc162f1aaa92d3
Michael Pheasant <mpheasant@gmail.com>
2016-05-24 11:23:36 +1000

    create undirected graph structure from JSON string

--- src/eph_clj/core.clj
+++ src/eph_clj/core.clj
@@ -1,6 +1,26 @@
-(ns eph-clj.core)
+(ns eph-clj.core
+  (:require [clojure.data.json :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-graph
+  "Given an input graph description g, create an undirected graph structure.
+Graph structure is a map of vertices to a map of adjacent vertices and distances,
+  eg. {:map [{:A {:B 10}}]} -> {:A {:B 10}, :B {:A 10}}."
+  [g]
+  (let [{edge-vec :map} g]
+    (and (vector? edge-vec)
+         (reduce (fn [g m] (let [[e1 edge-dist-map] (first m)]
+                             (reduce (fn [g' [e2 d]]
+                                       (-> g'
+                                           (assoc-in [e1 e2] d)
+                                           (assoc-in [e2 e1] d))) g
+                                     edge-dist-map))) {} edge-vec))))
+
+(defn read-json
+  "Read JSON string to clojure data, transforming any map key strings to keywords."
+  [s]
+  (json/read-str s :key-fn keyword))
+
+(defn read-json-graph
+  "Create an undirected graph structure from a json string."
+  [s]
+  (-> s read-json make-graph))
--- test/eph_clj/core_test.clj
+++ test/eph_clj/core_test.clj
@@ -2,6 +2,31 @@
   (:require [clojure.test :refer :all]
             [eph-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def s1 "{\"map\": [{\"A\": { \"B\": 100, \"C\": 30 }}, {\"B\": { \"F\": 300}}]}")
+(def m1 {:A {:B 100 :C 30 } :B {:A 100 :F 300} :C {:A 30} :F {:B 300}})
+
+(deftest test-json
+  (testing "read json"
+    (is (= {:a 1 :b 2} (read-json "{\"a\":1,\"b\":2}")))
+    (is (= {:a {:b 80}} (read-json "{\"a\": {\"b\": 80}}")))
+    (is (= {:start "a" :end "b"} (read-json "{\"start\": \"a\" \"end\": \"b\"}")))
+    (is (= {:map [{ :A {:I 70, :J 150} }]}
+           (read-json "{\"map\": [{\"A\": {\"I\":70, \"J\":150} }]}")))
+    (is (= [{:A {:B 100, :C 30}} {:B {:F 300}}]
+           (:map (read-json s1))))
+    (is (= {:A {:B 100, :C 30}}
+           (first (:map (read-json s1)))))
+    (is (= [:A {:B 100, :C 30}]
+           (first (first (:map (read-json s1))))))
+    (is (= {:B 100, :C 30}
+           (second (first (first (:map (read-json s1)))))))))
+
+(deftest test-make-graph
+  (testing "making graph"
+    (is (= {:B {:F 300} :F {:B 300}}
+           (make-graph {:map [{:B {:F 300}}]})))
+    (is (= m1 (make-graph {:map [{:A {:B 100, :C 30}} {:B {:F 300}}]})))))
+
+(deftest test-read-json-graph
+  (testing "making graph from json"
+    (is (= m1 (read-json-graph s1)))))
